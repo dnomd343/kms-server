d96bbeafa12420b6aec4a69d4efc3c633e12f176
Dnomd343 <i@343.re>
2022-10-22 14:50:23 +0800

    remove: localhost test code

--- src/Basis.php
+++ src/Basis.php
@@ -60,8 +60,7 @@ function getHost(): string {
         return 'KMS_HOST';
     }
     $host = v6DelBracket($match[1][0]); // try to remove ipv6 bracket again
-    // TODO: recover $host param
-//    $host = ($host == '127.0.0.1' or $host == '::1') ? 'KMS_HOST' : $host; // ignore localhost forward
+    $host = ($host == '127.0.0.1' or $host == '::1') ? 'KMS_HOST' : $host; // ignore localhost forward
     return (isHost($host)) ? $host : 'KMS_HOST';
 }
 
